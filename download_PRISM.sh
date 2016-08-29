75c80bc900523a774d30555a05e8f0bdc201523f
daviddralle <daviddralle@gmail.com>
2016-08-29 13:46:30 -0700

    Added script to unzip PRISM. Needs to be adapted for other folders

--- download_PRISM.sh
+++ download_PRISM.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+#input: 
+#	-start year as an integer
+#	-end year as an integer
+#	-directory in which to store data (e.g. 
+#	-which variable to download (must be a PRISM variable, e.g. 'ppt', 'tdmean', etc)
+
 START=$1
 STOP=$2
 DIR=$3
 CLIM=$4
 
+sudo mkdir $DIR/$CLIM
+chmod 777 $DIR/$CLIM
+
 for i in $(seq $START $STOP);
 do
 	VAR=$(curl ftp://prism.nacse.org/daily/$CLIM/$i/)
-	COMM=$DIR$i	
+	COMM=$DIR/$CLIM/$i	
 	echo $COMM
 	sudo mkdir $COMM
 	for j in $VAR
---
+++ unzip_all_prism.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 23 13:38:05 2016
+
+Script to unzip any PRISM files within 'raw_data' directory. 
+All data should be 'stable', daily PRISM data. Do not use
+provisional data. 
+
+@author: daviddralle
+"""
+
+import zipfile
+import os
+from os.path import dirname
+
+
+def main():
+    parent_dir = dirname(dirname(os.getcwd()))
+    raw_data_dir = os.path.join(parent_dir,'raw_data')
+    prism_vars = ['ppt','tmean','tmin','tmax','tdmean']
+    
+    for prism_var in prism_vars:
+        years = os.listdir(os.path.join(raw_data_dir,prism_var))
+        if len(years)==1:
+            print 'Warning: Could not find any folders containing ' + prism_var + ' data.'
+            continue
+            
+        for year in years:
+            try: 
+                int(year)
+            except ValueError:
+                continue
+                
+            print 'unzipping ' + year + ' ' + prism_var + ' data.'
+            zips=os.listdir(os.path.join(raw_data_dir,prism_var,year))
+            zips = [x for x in zips if '.zip' in x]
+            if len(zips)==0:
+                continue
+            else:
+                for zip_file in zips:
+                        if 'stable' in zip_file:
+                            zip_ref = zipfile.ZipFile(os.path.join(raw_data_dir,prism_var,year,zip_file), 'r')
+                            zip_ref.extractall(os.path.join(raw_data_dir,prism_var,year))
+                            zip_ref.close()
+                            os.remove(os.path.join(raw_data_dir,prism_var,year,zip_file))
+                        else:
+                            os.remove(os.path.join(raw_data_dir,prism_var,year,zip_file))
+        
+
+if __name__ == '__main__': 
+    main()
+
